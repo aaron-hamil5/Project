fd64059b1b76c1ac67ef660d99afe2c9bf281091
Aaron Hamilton (MacBook Air) <aaronleon28@gmail.com>
2026-03-27 11:20:47 +0000

    More comments

--- ChinookEntities.cs
+++ ChinookEntities.cs
@@ -1,25 +1,33 @@
 using System.ComponentModel.DataAnnotations;
+
+//This is the Albums table
 public class Album{
     public Int32 AlbumId{get; set;}
     public required String Title{get; set;}
     public Int32 ArtistId{get; set;}
 }
 
+//This is the Artists table
+
 public class Artist{
     public Int32 ArtistId{get; set;}
     public required String Name{get; set;}
 }
 
+//This is the Genres table
 public class Genre{
     public Int32 GenreId{get; set;}
     public required String Name{get; set;}
 }
 
+//This is the MediaTypes table
 public class MediaType{
     public Int32 MediaTypeId{get; set;}
     public required String Name{get; set;}
 }
 
+
+//This is the Tracks table
 public class Track{
     public Int32 TrackId{get; set;}
     public required String Name{get; set;}
@@ -32,8 +40,11 @@ public class Track{
     public Double UnitPrice{get; set;}
 }
 
+//This is the InvoiceItems table
 public class InvoiceItem
 {
+    //https://learn.microsoft.com/en-us/ef/core/modeling/keys?tabs=data-annotations
+    //Had to specify that the primary key is InvoiceLineId because it was not finding it.
     [Key]
     public Int32 InvoiceLineId {get; set;}
     public Int32 InvoiceId {get; set;}
@@ -42,19 +53,24 @@ public class InvoiceItem
     public Int32 Quantity {get; set;}
 }
 
+//This is the Playlists table
 public class Playlist
 {
     public Int32 PlaylistId {get; set;}
     public Int32 Name {get; set;}
 }
 
+//This is the PlaylistTracks table
 public class PlaylistTrack
 {
+    //https://learn.microsoft.com/en-us/ef/core/modeling/keys?tabs=data-annotations
+    //Had to specify that the primary key is PlaylistId because this table has 2 primary keys and didnt know which one to use.
     [Key]
     public Int32 PlaylistId { get; set; }
     public Int32 TrackId { get; set; }
 }
 
+//This is a class that has both Artist and Album tables for joining later.
 public class ArtistAlbum{
     public Int32 ArtistId{get; set;}
     public required String Name{get; set;}
@@ -62,6 +78,7 @@ public class ArtistAlbum{
     public required String Title{get; set;}
 }
 
+//This is a class that has both Album and Track tables for joining later.
 public class AlbumTrack{
     public Int32 AlbumId{get; set;}
     public required String Title{get; set;}
